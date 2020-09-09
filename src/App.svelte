0fb9f2cdf8f3f0157970b7081bc574260d1f684c
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-09-09 18:17:09 +0200

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import 'chota';
+  import { Container, Row, Col } from 'svelte-chota';
+</script>
+
+<main>
+  <Container>
+    <h1>Mój Brewlog</h1>
+    <Row>
+      <Col size="6">
+        <h2>Fermentuje</h2>
+      </Col>
+      <Col size="6">
+        <h2>Dojrzewa</h2>
+      </Col>
+    </Row>
+    <Row>
+      <Col size="6">
+        <h2>Wyszynk</h2>
+      </Col>
+      <Col size="6">
+        <h2>Receptury</h2>
+      </Col>
+    </Row>
+  </Container>
+</main>
