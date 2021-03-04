7e5d34acb181217164eda23785953f5329184a0b
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-03-04 21:12:10 +0100

    Nested components!

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,27 @@
 <script>
+  import Panel from './Panel.svelte';
 </script>
 
 <style lang="scss" global>
   @import '../node_modules/spectre.css/src/spectre.scss';
 </style>
 
-<div class="App">
+<div class="container grid-md">
   <h1>Mój Brewlog</h1>
+  <div class="columns">
+    <div class="column col-sm-6">
+      <Panel title={'Fermentuje'}/>
+    </div>
+    <div class="column col-sm-6">
+      <Panel title={'Dojrzewa'}/>
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column col-sm-6">
+      <Panel title={'Wyszynk'}/>
+    </div>
+    <div class="column col-sm-6">
+      <Panel title={'Receptury'}/>
+    </div>
+  </div>
 </div>
---
+++ src/Panel.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let title;
+</script>
+
+<div class="panel">
+  <div class="panel-header">
+    <div class="panel-title h5">{title}</div>
+  </div>
+</div>
